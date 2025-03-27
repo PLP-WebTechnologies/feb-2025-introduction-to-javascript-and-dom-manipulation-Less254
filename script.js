254cc79cf479aa45567a30d40806bb3fd8d81275
Less254 <146372701+Less254@users.noreply.github.com>
2025-03-27 21:00:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function hideOut() {
+    document.getElementById("web")
+    .style.display = "none";
+
+}
+
+function myFunction() {
+    document.getElementById("soft")
+    .innerHTML = "Hello World";
+    
+    document.getElementById("soft")
+    .style.color = "red";
+}
